f6e98228bc49e25a44d812c715f4a5e3a8b1bf89
sawkun <saru931aaa@yahoo.co.jp>
2020-10-28 04:40:36 +0000

    タスク一覧ページのurlをログイン中のユーザーIDを含んだ形で表示成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,13 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+  end
+  
   def destroy
     @user = User.find(params[:id])
     @user.destroy
